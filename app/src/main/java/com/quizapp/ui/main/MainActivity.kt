dd41c6a1eb0a2c0b39533104813be6895f07c028
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-09-08 07:05:25 +0700

    create splash screen

--- app/src/main/java/com/quizapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/quizapp/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.quizapp
+package com.quizapp.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.quizapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,6 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }                                                                                                           
     }
 }
---
+++ app/src/main/java/com/quizapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.quizapp.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.quizapp.R
+import com.quizapp.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        delayAndGotoMain()
+    }
+
+    private fun delayAndGotoMain() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menutup SplashActivity agar tidak kembali ke sini
+        }, 1200)
+    }
+}
